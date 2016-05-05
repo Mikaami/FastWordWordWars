76b1fdcf623ac13195ad0c7c633603900e8ccd82
martin almaraz <martin.almaraz@gmail.com>
2016-05-04 23:32:43 -0700

    100% bluetooth

--- app/src/main/java/com/gmail/martinalmaraz/fastwordwordwars/MainActivity.java
+++ app/src/main/java/com/gmail/martinalmaraz/fastwordwordwars/MainActivity.java
@@ -8,8 +8,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     public BluetoothAdapter myBlueTooth;
     int REQUEST_ENABLE_BLUETOOTH = 1;
     String NAME = "fastwordwordwars";
-    UUID MY_UUID = UUID.randomUUID();
+    UUID MY_UUID = UUID.fromString("d364b420-8d71-11e3-baa8-0800200c9a66");
+    //UUID MY_UUID = UUID.randomUUID();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,22 +55,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void startServer()
+    public void joinGame(View v)
     {
-        BluetoothSocket socket;
-
-        while(true)
+        Set<BluetoothDevice> devices = myBlueTooth.getBondedDevices();
+        BluetoothDevice other = null;
+        if(devices.size() > 0)
         {
-            try
+            for(BluetoothDevice b : devices)
             {
-                //socket =
-            }
-            catch (Exception e)
-            {
-                //
+                other = b;
+                Log.d("test", b.getAddress());
             }
         }
+        ConnectThread join = new ConnectThread(other);
+        join.run();
     }
+
+    public void hostGame(View v)
+    {
+        AcceptThread host = new AcceptThread();
+        host.run();
+        /*
+        new Thread(new Runnable()
+        {
+            public void run()
+            {
+                AcceptThread host = new AcceptThread();
+                host.run();
+            }
+        });
+        */
+
+    }
+
 
     private class AcceptThread extends Thread {
         private final BluetoothServerSocket mmServerSocket;
@@ -75,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
         public AcceptThread() {
             // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
             BluetoothServerSocket tmp = null;
             try {
                 // MY_UUID is the app's UUID string, also used by the client code
@@ -87,16 +108,18 @@ public class MainActivity extends AppCompatActivity {
             BluetoothSocket socket = null;
             // Keep listening until exception occurs or a socket is returned
             while (true) {
+                Log.d("run", "going");
                 try {
                     socket = mmServerSocket.accept();
                 } catch (IOException e) {
+                    Log.d("run", "failed accept");
                     break;
                 }
                 // If a connection was accepted
                 if (socket != null) {
                     // Do work to manage the connection (in a separate thread)
                     //manageConnectedSocket(socket);
-                    Log.d("test", "connected!");
+                    Log.d("test", "connected! Accept Thread");
                     try
                     {
                         mmServerSocket.close();
@@ -135,6 +158,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 // MY_UUID is the app's UUID string, also used by the server code
                 tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                if(tmp != null)
+                    Log.d("test", "here1");
             } catch (IOException e) { }
             mmSocket = tmp;
         }
@@ -142,12 +167,12 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             // Cancel discovery because it will slow down the connection
             mBluetoothAdapter.cancelDiscovery();
-
+            Log.d("run", "going");
             try {
                 // Connect the device through the socket. This will block
                 // until it succeeds or throws an exception
                 mmSocket.connect();
-                Log.d("test", "connected!");
+                Log.d("test", "connected! Connect Thread");
             } catch (IOException connectException) {
                 // Unable to connect; close the socket and get out
                 try {
