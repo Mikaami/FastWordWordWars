23942ccef10fee2fdf141d1d13b136cf4b774312
martin almaraz <martin.almaraz@gmail.com>
2016-05-04 21:04:26 -0700

    Got BT working2

---
+++ app/src/main/java/com/gmail/martinalmaraz/fastwordwordwars/MainActivity.java
@@ -0,0 +1,54 @@
+package com.gmail.martinalmaraz.fastwordwordwars;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter myBlueTooth;
+    int REQUEST_ENABLE_BLUETOOTH = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupBlue();
+    }
+
+    @Override
+    protected void  onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH && resultCode == RESULT_OK)
+        {
+            Log.d("test", "did it");
+        }
+        else
+            Log.d("test", "didnt do it");
+    }
+
+    public void setupBlue()
+    {
+        myBlueTooth = BluetoothAdapter.getDefaultAdapter();
+        if(myBlueTooth == null)
+        {
+            Log.d("fail", "doesnt support bluetooth");
+        }
+
+        if (!myBlueTooth.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
+    }
+
+    public void sendData()
+    {
+        //oifoewifnoeifnofinefowfnwef
+        //woifnwo
+    }
+
+
+
+}
